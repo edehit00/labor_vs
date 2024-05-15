6945a2a03df46004631039c439f0c705695e9631
Edgar Ehremann <edehit00@hs-esslingen.de>
2024-05-15 18:38:20 +0200

    Initialize maven project

---
+++ src/main/java/com/edehit00/vslabor/VsLaborApplication.java
@@ -0,0 +1,13 @@
+package com.edehit00.vslabor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VsLaborApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VsLaborApplication.class, args);
+	}
+
+}
